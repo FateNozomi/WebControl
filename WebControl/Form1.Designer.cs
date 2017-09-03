cf434e5d2f79c0288844e92b2276eda6e61f70ba
Fate <wailinnyu@gmail.com>
2017-09-03 10:09:10 +0800

    Initial commit

---
+++ WebControl/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace WebControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wbMap = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // wbMap
+            // 
+            this.wbMap.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wbMap.Location = new System.Drawing.Point(0, 0);
+            this.wbMap.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbMap.Name = "wbMap";
+            this.wbMap.Size = new System.Drawing.Size(1264, 321);
+            this.wbMap.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 321);
+            this.Controls.Add(this.wbMap);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser wbMap;
+    }
+}
+
---
+++ WebControl/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebControl
+{
+    public partial class Form1 : Form
+    {
+        uint rows = 6;
+        uint columns = 36;
+
+        public Form1()
+        {
+            InitializeComponent();
+            constructResult();
+        }
+
+        // Empty Map for reference
+        private void loadEmptyMap()
+        {
+            StringBuilder sb2 = new StringBuilder();
+            sb2.AppendLine("<html>");
+            sb2.AppendLine("<body>");
+            sb2.AppendLine("<table border='0' width=100% height=100%>");
+
+            sb2.AppendLine("<tr height=1%>");
+            for (uint i = 0; i < columns + 1; i++)
+            {
+                if (i == 0)
+                {
+                    sb2.AppendLine("<td width=1%></td>");
+                }
+                else
+                {
+                    sb2.AppendLine("<td align=center><font size=2>" + i + "</font></td>");
+                }
+            }
+            sb2.AppendLine("</tr>");
+
+            for (uint i = 0; i < rows; i++)
+            {
+                sb2.AppendLine("<tr>");
+                for (uint j = 0; j < columns; j++)
+                {
+                    if (j == 0)
+                    {
+                        sb2.AppendLine("<td align=center><font size=2>" + (rows - i) + "</font></td>");
+                    }
+                    sb2.AppendLine("<td align=center width=" + (100 / columns) + "% ");
+                    sb2.Append("bgcolor='lightgrey'>");
+                    sb2.AppendLine("</td>");
+                }
+                sb2.AppendLine("</tr>");
+            }
+            sb2.AppendLine("</table>");
+            sb2.AppendLine("</body>");
+            sb2.AppendLine("</html>");
+            wbMap.DocumentText = sb2.ToString();
+            wbMap.Document.Write(sb2.ToString());
+        }
+
+        private void constructResult(string text = "", string mag = "")
+        {
+            uint status = 0;
+            string rc = "0"; // Reject Code
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("<html>");
+            sb.AppendLine("<body>");
+            sb.AppendLine("<table border='0' width=100% height=100%>");
+
+            sb.AppendLine("<tr height=1%>");
+            for (uint i = 0; i < columns + 1; i++)
+            {
+                if (i == 0)
+                {
+                    sb.AppendLine("<td width=1%></td>");
+                }
+                else
+                {
+                    sb.AppendLine("<td align=center><font size=2>" + i + "</font></td>");
+                }
+            }
+            sb.AppendLine("</tr>");
+
+            for (uint i = 0; i < rows; i++)
+            {
+                sb.AppendLine("<tr>");
+                for (uint j = 0; j < columns; j++)
+                {
+                    if (j == 0)
+                    {
+                        sb.AppendLine("<td align=center><font size=2>" + (rows - i) + "</font></td>");
+                    }
+                    sb.AppendLine("<td align=center width=" + (100 / columns) + "% ");
+                    int[] unitCoord = GetUnitCoord(i, j);
+                    sb.Append("title='Unit #" + unitCoord[1] + "," + unitCoord[0] + "' ");
+                    if (status == 0)
+                    {
+                        sb.Append("bgcolor='green'>" + rc.ToString());
+                    }
+                    else
+                    {
+                        sb.Append("bgcolor='red'>" + rc.ToString());
+                    }
+
+                    sb.AppendLine("</td>");
+                }
+                sb.AppendLine("</tr>");
+            }
+            sb.AppendLine("</table>");
+            sb.AppendLine("</body>");
+            sb.AppendLine("</html>");
+            wbMap.DocumentText = sb.ToString();
+        }
+
+        private int[] GetUnitCoord(uint r, uint c)
+        {
+            int x = (int)c;
+            int y = (int)(rows - r);
+            y--;
+
+            int[] unit = new int[2];
+
+            // Start from Index of 1 instead of 0
+            unit[0] = ++y;
+            unit[1] = ++x;
+
+            return unit;
+        }
+    }
+}
